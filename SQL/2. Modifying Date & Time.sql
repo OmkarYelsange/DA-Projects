da1411cfe80ed46cfd6ccb37e44c93e8099eb92c
Omkar Yelsange <omkaryelsange1010@gmail.com>
2026-08-02 19:21:58 +0530

    Starting With SQL

---
+++ SQL/2. Modifying Date & Time.sql
@@ -0,0 +1,25 @@
+# Modifying Timing and Separating Date & Time 
+SELECT Timing FROM Sensor_Data LIMIT 10;
+
+ALTER TABLE Sensor_Data
+ADD COLUMN Date_Col DATE,
+ADD COLUMN Time_Col TIME;
+
+UPDATE Sensor_Data
+SET 
+	Date_Col = DATE(STR_TO_DATE(Timing,'%d-%m-%Y %H:%i:%s')),
+	Time_Col = TIME(STR_TO_DATE(Timing,'%d-%m-%Y %H:%i:%s'));
+
+ALTER TABLE Sensor_Data
+MODIFY COLUMN Date_Col DATE AFTER Timing;
+
+ALTER TABLE Sensor_Data
+MODIFY COLUMN Time_Col TIME AFTER Date_Col;
+
+ALTER TABLE Sensor_Data
+CHANGE Date_Col `Date` DATE;
+
+ALTER TABLE Sensor_Data
+CHANGE Time_Col `Time` TIME;
+
+
